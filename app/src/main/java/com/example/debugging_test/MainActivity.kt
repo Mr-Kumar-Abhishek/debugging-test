e592e49b600e787ce45b92f20ae1d248a1479d1c
Abhishek Kumar <mr.kumar.abhishek@email.com>
2022-06-21 15:51:13 +0530

    include a test bug

--- app/src/main/java/com/example/debugging_test/MainActivity.kt
+++ app/src/main/java/com/example/debugging_test/MainActivity.kt
@@ -11,7 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         logging()
+        division()
     }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+
     fun logging() {
         Log.e(TAG, "ERROR: a serious error like an app crash")
         Log.w(TAG, "WARN: warns about the potential for serious errors")
